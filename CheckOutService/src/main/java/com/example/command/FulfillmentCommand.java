e99da91d4e30075cc40bbc208e50b23a063bc05b
Harianantha <hariharan.anantharaman@gmail.com>
2016-03-01 21:53:21 +0530

    changes for hystrix

--- CheckOutService/src/main/java/com/example/command/FulfillmentCommand.java
+++ CheckOutService/src/main/java/com/example/command/FulfillmentCommand.java
@@ -4,12 +4,13 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.InetSocketAddress;
 import java.net.MalformedURLException;
+import java.net.Proxy;
 import java.net.URL;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import com.example.CheckoutController;
 import com.example.data.Order;
 import com.netflix.hystrix.HystrixCommand;
 import com.netflix.hystrix.HystrixCommandGroupKey;
@@ -30,6 +31,7 @@ public class FulfillmentCommand extends HystrixCommand<Order> {
 		// TODO Auto-generated method stub
 		LOGGER.entering(CLASSNAME, methodName);
 		invokeFullfillmentService();
+		order.setStatus(order.getOrderId()+":"+"Deleivered");
 		LOGGER.exiting(CLASSNAME, methodName);
 		return order;
 	}
@@ -40,10 +42,15 @@ public class FulfillmentCommand extends HystrixCommand<Order> {
 		  try {
 
 			URL url = new URL(URL_PATH);
+			long startTime=System.currentTimeMillis();
+			//HttpHost proxy = new HttpHost("proxy.cognizant.com");
+			Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("proxy.cognizant.com", 6050));
 			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-			conn.setRequestMethod("POST");
+			conn.setRequestMethod("GET");
 			conn.setRequestProperty("Accept", "application/json");
-
+			conn.addRequestProperty("http.proxyHost", "proxy.cognizant.com");
+			conn.addRequestProperty("http.proxyPort", "6050");
+			conn.addRequestProperty("java.net.useSystemProxies", "true");
 			if (conn.getResponseCode() != 200) {
 				throw new RuntimeException("Failed : HTTP error code : "
 						+ conn.getResponseCode());
@@ -57,7 +64,8 @@ public class FulfillmentCommand extends HystrixCommand<Order> {
 			while ((output = br.readLine()) != null) {
 				System.out.println(output);
 			}
-
+			long endtime=System.currentTimeMillis();
+			System.out.println("Time to call fulfillment service:"+(endtime-startTime));
 			conn.disconnect();
 
 		  } catch (MalformedURLException e) {
