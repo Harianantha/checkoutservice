59cc93103c210302d731067be012117bcf84d1c8
Harianantha <hariharan.anantharaman@gmail.com>
2016-03-01 21:52:35 +0530

    changes for hystrix

--- CheckOutService/src/main/java/com/example/CheckoutController.java
+++ CheckOutService/src/main/java/com/example/CheckoutController.java
@@ -1,5 +1,7 @@
 package com.example;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 import java.util.logging.Logger;
 
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,16 +11,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.command.FulfillmentCommand;
 import com.example.data.Order;
+import com.netflix.hystrix.HystrixCommandProperties;
 
 
 @RestController
-@RequestMapping("/order/checkout")
+
 public class CheckoutController {
 	private final static Logger LOGGER = Logger.getLogger(CheckoutController.class.getName());
 	 private final static String CLASSNAME="CheckoutController";
    
-
-    @RequestMapping(method = RequestMethod.GET)
+	
+    @RequestMapping(path="/order/checkout",method = RequestMethod.GET)
     public String getStatus() {
     	
     	return "DELIVERED";
@@ -38,17 +41,33 @@ public class CheckoutController {
     	//return "DELIVERED";
         
     }
-    @RequestMapping(value = "{id}", method = RequestMethod.GET)
+    @RequestMapping(path="/status/{id}", method = RequestMethod.GET)
+    //@RequestMapping("/status",value = "{id}", method = RequestMethod.GET)
     public String placeOrderTest(@PathVariable("id") long id) {
     	String methodName="placeOrderTest";
     	LOGGER.entering(CLASSNAME, methodName);
     	Order order=new Order();
+    	order.setOrderId(Long.toString(id));
     	FulfillmentCommand command=new FulfillmentCommand(order);
-    	Order returnVal=(Order)command.execute();
-    	
+    	HystrixCommandProperties.Setter().withExecutionTimeoutInMilliseconds(10000);
+    	//Order returnVal=(Order)command.
+    	Future<Order> future=command.queue();
+    	Order returnVal=null;
+		try {
+			returnVal = future.get();
+		} catch (InterruptedException e)  {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}catch(ExecutionException e){
+			e.printStackTrace();
+		}
     	LOGGER.exiting(CLASSNAME, methodName);
     	
-    	return "Order Placed Successfully"+System.currentTimeMillis();
+    	if(returnVal!=null)
+    	return returnVal.getStatus();
+    	else
+    		return "Fulfillment Service Invocation failed";
+    	//return "Order Placed Successfully"+System.currentTimeMillis();
     	//return "DELIVERED";
         
     }
