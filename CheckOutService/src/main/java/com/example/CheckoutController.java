d7b389b9aa34db1d664e9d927b639d68fa7ea64f
Harianantha <hariharan.anantharaman@gmail.com>
2016-03-03 08:45:42 +0530

    swagger changes

--- CheckOutService/src/main/java/com/example/CheckoutController.java
+++ CheckOutService/src/main/java/com/example/CheckoutController.java
@@ -13,6 +13,12 @@ import com.example.command.FulfillmentCommand;
 import com.example.data.Order;
 import com.netflix.hystrix.HystrixCommandProperties;
 
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 
 @RestController
 
@@ -20,7 +26,14 @@ public class CheckoutController {
 	private final static Logger LOGGER = Logger.getLogger(CheckoutController.class.getName());
 	 private final static String CLASSNAME="CheckoutController";
    
-	
+	 @ApiOperation(value = "getStatus", nickname = "getStatus")
+	   
+	@ApiResponses(value = { 
+	@ApiResponse(code = 200, message = "Success", response = String.class),
+	@ApiResponse(code = 401, message = "Unauthorized"),
+	@ApiResponse(code = 403, message = "Forbidden"),
+	@ApiResponse(code = 404, message = "Not Found"),
+	@ApiResponse(code = 500, message = "Failure")}) 
     @RequestMapping(path="/order/checkout",method = RequestMethod.GET)
     public String getStatus() {
     	
